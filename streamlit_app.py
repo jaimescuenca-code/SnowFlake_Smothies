e27e9ab217e997853ce4ca6705883535f53200fc
jaimescuenca-code <jaime.s.cuenca@bluetab.net>
2026-03-11 14:01:19 +0100

    Update streamlit_app.py
    Connected changes need for SnS from SiS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -47,11 +47,12 @@ st.write(
 
 # st.subheader("Underlying data")
 # st.dataframe(queried_data, use_container_width=True)
+cnx = st.connection('Snowflake')
 
 name_of_order = st.text_input('Name on Smoothie:')
 st.write('Your name on Smoothie will be:', name_of_order)
 
-session = get_active_session()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 

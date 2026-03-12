6b42a86f7775cc89c71466128a02e7bd065d9d12
jaimescuenca-code <jaime.s.cuenca@bluetab.net>
2026-03-12 10:09:18 +0100

    Update streamlit_app.py
    API Response 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ ingredients_list = st.multiselect(
     max_selections = 5
 )
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response)
 sf_df = st.dataframe(smoothiefroot_response.json(), user_container_width=True)
 
